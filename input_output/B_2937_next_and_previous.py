732463557c6e53bea0c17646d2d358fca1c46ee1
zelgadina <zelgadina@yandex.ru>
2020-04-24 19:00:07 +0300

    add B_task

---
+++ input_output/B_2937_next_and_previous.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+number = int(input())
+print('The next number for the number ', number, ' is ', number + 1, '.', sep='')
+print('The previous number for the number ', number, ' is ', number - 1, '.', sep='')
